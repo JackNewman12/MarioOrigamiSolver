d11c75293eed48d029789b4cc5251048ab91f8f5
Jack Newman <jack.newman12@gmail.com>
2020-07-20 09:56:29 +0930

    Better algo

--- solver.py
+++ solver.py
@@ -1,23 +1,29 @@
 import copy
 import numpy as np
 import time
-
-gSeenBoards = set()
-gPerfectMoves = False
+import math
 
 class GameBoard:
     """
     The Gameboard State
     """
 
-    def __init__(self, Board=None, NumMoves=1, NumAttacks=1) -> None:
+    def __init__(self, Board=None, NumMoves=None, NumAttacks=None) -> None:
         if Board is not None:
             self.Board = np.array(Board)
         else:
             self.Board = np.zeros((4, 12), dtype=int)
         assert self.Board.shape == (4, 12)
-        self.NumMoves = NumMoves
-        self.NumAttacks = NumAttacks
+        
+        if NumMoves:
+            self.NumMoves = NumMoves
+        else:
+            self.NumMoves = 3 # math.ceil(self.NumEnemies() / 4)
+        
+        if NumAttacks:
+            self.NumAttacks = NumAttacks
+        else:
+            self.NumAttacks = math.ceil(self.NumEnemies() / 4)
 
     def MoveRow(self, row, num):
         assert 0 <= row <= 3
@@ -56,121 +62,90 @@ def SolveBoard(Board: GameBoard, NumMoves=None):
     Solve any given board
     """
     Moves = []
-    if not NumMoves:
+    if NumMoves is None:
         # Initial Solve - Get from board def
         NumMoves = Board.NumMoves
-    NumMoves -= 1
+
     # Rows
     for i in range(0, 4):
-        for j in range(0, 12):
+        for j in range(1, 12): # Start at once since move of 0 is wasteful
             TempBoard: GameBoard = copy.deepcopy(Board)
             TempBoard.MoveRow(i, j)
-            Hash = TempBoard.Hash()
-            if Hash in gSeenBoards:
-                continue
-            gSeenBoards.add(Hash)
             Moves.append({"Board": TempBoard, "Move": "Row", "Loc": i, "Shift": j})
     # Cols
     for i in range(0, 6):  # Only need to do 6 out of twelve since they are connected
-        for j in range(0, 8):
+        for j in range(1, 8): # Start at once since move of 0 is wasteful
             TempBoard: GameBoard = copy.deepcopy(Board)
             TempBoard.MoveCol(i, j)
-            Hash = TempBoard.Hash()
-            if Hash in gSeenBoards:
-                continue
-            gSeenBoards.add(Hash)
             Moves.append({"Board": TempBoard, "Move": "Col", "Loc": i, "Shift": j})
 
     if not Moves:
-        return []
+        return False, []
 
     for Move in Moves:
-        BestAttacks = SimAttack(Move["Board"])
-        if BestAttacks:
-            Move["Attacks"] = BestAttacks
-            Move["EnemiesLeft"] = BestAttacks[-1]["Enemies"]
-
-    # Find the board that would result in the least enemies unattacked
-    # (and then my number of attacks required)
-    Moves = sorted(
-        Moves,
-        key=lambda k: (k.get("EnemiesLeft", 999), len(k.get("Attacks", range(10)))),
-    )
-    if NumMoves == 0:
-        return [Moves[0]]
+        Win, _ = SimAttack(Move["Board"])
+        if Win:
+            return True, [Move]
     
-    Results = []
-    for Move in Moves:
-        Result = [Move]
-        Result.extend(SolveBoard(Move["Board"], NumMoves))
-        if Result[-1].get('EnemiesLeft',999) == 0:
-            return Result
-        Results.append(Result)
-    Results = sorted(
-        Results,
-        key=lambda k: (
-            k[-1].get("EnemiesLeft", 999),
-            len(k[-1].get("Attacks", range(10))),
-        ),
-    )
-    return Results[0]
+    # Search for a minimal number of moves solution first
+    for Num in range(1,NumMoves):
+        for Move in Moves:
+            Win, SubMoves = SolveBoard(Move["Board"], Num)
+            if Win:
+                Result = [Move]
+                Result.extend(SubMoves)
+                return True, Result
+
+    return False, []
 
 
-def SimAttack(Board: GameBoard, NumAttacks=None):
+def SimAttack(Board: GameBoard):
     """
     Simulate all the possible attacks
     """
-    if not NumAttacks:
-        # Inital Sim - Get it from the board
-        NumAttacks = Board.NumAttacks
-    Attacks = []
-    NumAttacks -= 1
     if Board.NumEnemies() == 0:
-        return []
+        return False, []
 
+    TempBoard: GameBoard = copy.deepcopy(Board)
+    BootAttacks = []
+    # Boots
     for i in range(0, 12):
-        for j in ["Hammer", "Boot"]:
-            TempBoard: GameBoard = copy.deepcopy(Board)
-            StartEnemies = TempBoard.NumEnemies()
-            if j == "Hammer":
-                TempBoard.AtkHammer(i)
-            if j == "Boot":
-                TempBoard.AtkBoot(i)
-            FinishEnemies = TempBoard.NumEnemies()
-            if FinishEnemies < StartEnemies:
-                if gPerfectMoves and not (StartEnemies - FinishEnemies == 4):
-                    continue
-                Attacks.append(
-                    {"Move": j, "Col": i, "Board": TempBoard, "Enemies": FinishEnemies}
-                )
+        if TempBoard.Board[0][i] or TempBoard.Board[1][i]:
+            TempBoard.AtkBoot(i)
+            BootAttacks.append(
+                {"Move": "Boot", "Col": i}
+            )
 
-    if not Attacks:
-        return []
-    Attacks = sorted(Attacks, key=lambda k: k["Enemies"])
-    if NumAttacks == 0:
-        return [Attacks[0]]
-    Results = []
-    for Atk in Attacks:
-        Result = [Atk]
-        Result.extend(SimAttack(Atk["Board"], NumAttacks))
-        if Result[-1]["Enemies"] == 0:
-            return Result
-        Results.append(Result)
-    # Simulated a bunch of moves - Choose best to pass up
-    Results = sorted(Results, key=lambda k: (k[-1]["Enemies"], len(k)))
-    return Results[0]
+    HammerAttacksBoard = copy.deepcopy(TempBoard)
+    HammerAttacks = []
+    # Hammer
+    for i in range(0, 12):
+        if HammerAttacksBoard.Board[2][i] or HammerAttacksBoard.Board[3][i]:
+            HammerAttacksBoard.AtkHammer(i)
+            HammerAttacks.append(
+                {"Move": "Hammer", "Col": i}
+            )
+    if (len(HammerAttacks) + len(BootAttacks)) <= Board.NumAttacks:
+        return True, BootAttacks.extend(HammerAttacks)
+    
+    HammerAttacksBoard = copy.deepcopy(TempBoard)
+    HammerAttacks = []
+    # Hammer - Shifted to cover edge cases
+    for i in range(-1, -13, -1):
+        if HammerAttacksBoard.Board[2][i] or HammerAttacksBoard.Board[3][i]:
+            HammerAttacksBoard.AtkHammer(i)
+            HammerAttacks.append(
+                {"Move": "Hammer", "Col": i}
+            )
+    if (len(HammerAttacks) + len(BootAttacks)) <= Board.NumAttacks:
+        return True, BootAttacks.extend(HammerAttacks)
 
-def PrintResult(Result):
-    for Move in Result:
+    return False, []
+
+def PrintResult(Moves):
+    for Move in Moves:
         print(f"Move {Move['Move']} {Move['Loc']} by {Move['Shift']}")
-
-    LastMove = Result[-1]
-    Attacks = LastMove["Attacks"]
-    for Attack in Attacks:
-        print(f"Attack Col {Attack['Col']} with {Attack['Move']}")
-
-    print(f"Result leaves {LastMove['EnemiesLeft']} enemies untouched")
-    print(f"Final Board State:\n{LastMove['Board']}")
+    print(f"Final Board State:\n{Moves[-1]['Board']}")
 
 
 if __name__ == "__main__":
@@ -182,27 +157,25 @@ if __name__ == "__main__":
         Input = input()
         assert len(Input) == 12, "Uhh wrong length?"
         InputPos += Input
+#     InputPos = "\
+# 000000010000\
+# 000000000010\
+# 010000110000\
+# 010000110000"
+#     InputPos = "\
+# 000000010000\
+# 000000000010\
+# 100001100000\
+# 010000110000"
     
-    # I think the game calculates these two values as ceil(enemies/4) but not sure
-    # might be able to skip this question (and any weird extra calcs)
-    NumMoves = int(input("Number Of Moves: "))
-    NumAttacks = int(input("Number Of Attacks: "))
-
     InputPos = np.array([int(c) for c in InputPos])
     InputPos.resize((4, 12))
 
     StartTime = time.time()
-    Board = GameBoard(InputPos, NumMoves=NumMoves, NumAttacks=NumAttacks)
+    Board = GameBoard(InputPos)#, NumMoves=NumMoves, NumAttacks=NumAttacks)
     print(f"Here we go! Starting Board:\n{Board}")
     
-    # TODO - Is this good? Perfect moves might remove some possible answers?
-    # Currently it gives huge speed improvements 
-    if Board.NumEnemies() % 4 == 0:
-        print("Assuming perfect 4 attacks")
-        gPerfectMoves = True
-
-    gSeenBoards.add(hash(str(Board.Board)))
-    Result = SolveBoard(Board)
-    if Result:
-        PrintResult(Result)
+    Win, Moves = SolveBoard(Board)
+    if Win:
+        PrintResult(Moves)
     print(f"Finished: {time.time() - StartTime} seconds")
